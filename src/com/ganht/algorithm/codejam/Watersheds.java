aeb63e083fcec94e8c4deefa7708f690d1cda17f
ganhaitian <ganhaitian@gmail.com>
2014-03-31 21:00:53 +0800

    watershed done

--- src/com/ganht/algorithm/codejam/Watersheds.java
+++ src/com/ganht/algorithm/codejam/Watersheds.java
@@ -1,6 +1,7 @@
 package com.ganht.algorithm.codejam;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,11 @@ public class Watersheds extends CodeJamCase {
 
   @Override
   protected void runCase() {
-    parseInput(new File(""));
+    parseInput(new File("G:\\dep\\code-jam-case\\watersheds-small.in"));
+  }
+
+  public static void main(String[] args) {
+    new Watersheds().runCase();
   }
 
   public int NORTH = 0, WEST = 1, EAST = 2, SOUTH = 3;
@@ -64,64 +69,97 @@ public class Watersheds extends CodeJamCase {
       if (lowestCell != this) {
         this.to = lowestCell;
         lowestCell.from = this;
-        lowestCell.divide();
-
-        if (lowestCell.isSink) {
-          this.label = lowestCell.label;
+        
+        if(lowestCell.label == null){
+          lowestCell.divide();
         }
+          
+        this.label = lowestCell.label;
+       
       } else {
-        char currentLabel = (char) (globalLabel + 1);
+        globalLabel = (char) (globalLabel + 1);
+        char currentLabel = globalLabel;
         this.isSink = true;
         this.label = String.valueOf(currentLabel);
       }
+    }
+
+    public String toString() {
+      return String.format("Cell:{altitude:%d,labe:%s}", this.altitude, this.label);
     }
 
   }
 
   protected void acceptInput(CaseInputReader caseInputReader) {
 
-    // int caseNumber = Integer.parseInt(caseInputReader.readOneLine());
+    int caseNumber = Integer.parseInt(caseInputReader.readOneLine());
     String tmpCaseInfo = caseInputReader.readOneLine();
-    List<Cell> lastCases = new ArrayList<Cell>();
+    List<Cell> lastLines = new ArrayList<Cell>();
     List<Cell> allCells = new ArrayList<Cell>();
     int caseIndex = 0;
 
     while (tmpCaseInfo != null) {
       String[] caseInfo = tmpCaseInfo.split(" ");
       int h = Integer.parseInt(caseInfo[0]);
+      int w = Integer.parseInt(caseInfo[1]);
 
       List<String> lines = caseInputReader.readLines(h);
-      for (String line : lines) {
-        String[] altitudes = line.split(" ");
-        Cell lastCell = null;
-        for (int index = 0; index < altitudes.length; index++) {
-          Cell cell = new Cell(Integer.parseInt(altitudes[index]));
+      //while (lines != null) {
+        for (String line : lines) {
+          String[] altitudes = line.split(" ");
+          Cell lastCell = null;
+          for (int index = 0; index < altitudes.length; index++) {
+            Cell cell = new Cell(Integer.parseInt(altitudes[index]));
 
-          if (lastCell != null) {
-            cell.setNeighbor(WEST, lastCell);
-            lastCell.setNeighbor(EAST, cell);
-          }
-          lastCell = cell;
+            if (lastCell != null) {
+              cell.setNeighbor(WEST, lastCell);
+              lastCell.setNeighbor(EAST, cell);
+            }
+            lastCell = cell;
 
-          if (lastCases.size() > 0) {
-            lastCases.get(index).setNeighbor(SOUTH, cell);
-            cell.setNeighbor(NORTH, lastCases.get(index));
-            lastCases.set(index, cell);
+            if (lastLines.size() < w) {
+              lastLines.add(cell);
+            } else {
+              lastLines.get(index).setNeighbor(SOUTH, cell);
+              cell.setNeighbor(NORTH, lastLines.get(index));
+              lastLines.set(index, cell);
+            }
+            allCells.add(cell);
           }
-          allCells.add(cell);
+
         }
 
-        System.out.printf("Case #%d:/n", ++caseIndex);
+        System.out.printf("Case #%d:\n", ++caseIndex);
+        int tmpCellIndex = 0;
 
         for (Cell cell : allCells) {
           if (cell.label == null) {
             cell.divide();
-          } else {
-            System.out.print(cell.label);
+          }
+
+          System.out.print(cell.label);
+
+          if (++tmpCellIndex == w) {
+            System.out.println("");
+            tmpCellIndex = 0;
+          }else{
+            System.out.print(" ");
           }
         }
+
+        allCells.clear();
+        lastLines.clear();
+        globalLabel = 'a' - 1;
+        
+        tmpCaseInfo = caseInputReader.readOneLine();
       }
-    }
+
+      try {
+        caseInputReader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    //}
 
 
   }
