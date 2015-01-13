ef3478bb0e12b840a474821b2b48f839232a47ce
ganhaitian <ganhaitian@gmail.com>
2015-01-13 22:56:22 +0800

    纸牌游戏谜题上传

--- src/com/ganht/algorithm/codejam/ParenthesesOrder.java
+++ src/com/ganht/algorithm/codejam/ParenthesesOrder.java
@@ -12,64 +12,73 @@ public class ParenthesesOrder extends CodeJamCase {
 
     }
 
+    public static String result = null;
+
     public static void main(String[] args) {
         new ParenthesesOrder().parseInput(
-            new File("C:\\Users\\gan\\Downloads\\D-small-practice.in"),
+            new File("C:\\Users\\gan\\Downloads\\D-large-practice.in"),
             new InputCaseLineParser() {
                 @Override
                 public void parseLine(int lineNumber, String line) {
-                    if(lineNumber == 1)
+                    if (lineNumber == 1)
                         return;
                     String[] linePart = line.split(" ");
                     int n = Integer.parseInt(linePart[0]);
-                    int k = Integer.parseInt(linePart[1]);
-                    String kthStr = getKthSmallest(n,k);
-                    System.out.println(String.format("Case #%d: %s",lineNumber,kthStr));
+                    long k = Long.parseLong(linePart[1]);
+                    String kthStr = getKthSmallest(n, k);
+                    System.out.println(String.format("Case #%d: %s", lineNumber - 1, kthStr));
                 }
             });
     }
 
-    public static String getKthSmallest(int n,int k){
+    public static String getKthSmallest(int n, long k) {
         StringBuffer tmpStr = new StringBuffer();
-        StringBuffer result = new StringBuffer();
-        int sum = d(n,n,k,0,tmpStr,result);
-        if(sum != -1 && sum < k)
-            return "Doesn't Exist!";
+        try {
+            int sum = d(n, n, n, k, 0, 0, tmpStr);
+            if (sum < k)
+                return "Doesn't Exist!";
+        } catch (Exception e) {
+
+        }
         return result.toString();
     }
 
-    public static int d(int n1,int n2,int k,int accu,StringBuffer tmpValue,StringBuffer result){
+    public static int d(int n, int n1, int n2, long k, int accu, int accuSum, StringBuffer tmpValue) {
 
-        if(n1 == 0){
-            for(int i = 0;i < n2;i++)
+        if (n1 == 0) {
+            for (int i = 0; i < n2; i++)
                 tmpValue.append(")");
-            result.append(tmpValue);
+            result = tmpValue.toString();
             return 1;
         }
 
-        if(n2 == 0){
-            for(int i = 0;i < n1;i++)
+        if (n2 == 0) {
+            for (int i = 0; i < n1; i++)
                 tmpValue.append("(");
-            result.append(tmpValue);
+            result = tmpValue.toString();
             return 1;
         }
 
         StringBuffer tmpBuffer = new StringBuffer(tmpValue);
         int sum = 0;
-        if(accu <= n1)
-            sum += d(n1 - 1,n2,k,accu + 1, tmpBuffer.append("("),result);
-
-        if(sum == k){
-            return k;
+        if (accu <= n) {
+            sum += d(n, n1 - 1, n2, k, accu + 1, accuSum, tmpBuffer.append("("));
+            accuSum += sum;
         }
 
-        if(accu - 1 >= 0){
+        if (accuSum == k) {
+            throw new IllegalStateException();
+        }
+
+        if (accu - 1 >= 0) {
             tmpBuffer = new StringBuffer(tmpValue);
-            sum += d(n1,n2-1,k,accu - 1, tmpBuffer.append(")"),result);
+            int t = d(n, n1, n2 - 1, k, accu - 1, accuSum, tmpBuffer.append(")"));
+            sum += t;
+            accuSum += t;
         }
 
-        if( sum == k) {
-            return k;
+        if (accuSum == k) {
+            throw new IllegalStateException();
         }
 
         return sum;
