09f8738956235f63d242dd276f76508c4c9daf54
ganhaitian <ganhaitian@gmail.com>
2015-06-04 17:57:03 +0800

    tetris done111

---
+++ src/com/ganht/algorithm/codejam/CounterCulture.java
@@ -0,0 +1,14 @@
+package com.ganht.algorithm.codejam;
+
+/**
+ * In the Counting Poetry Slam, a performer takes the microphone, chooses a number N,
+ * and counts aloud from 1 to N. That is, she starts by saying 1, and then repeatedly
+ * says the number that is 1 greater than the previous number she said, stopping after
+ * she has said N.
+ * Created by ganhaitian on 2015/6/4.
+ */
+public class CounterCulture {
+
+
+
+}
---
+++ src/com/ganht/algorithm/introduction/Tree.java
@@ -0,0 +1,11 @@
+package com.ganht.algorithm.introduction;
+
+/**
+ * 树相关
+ * Created by ganhaitian on 2015/6/4.
+ */
+public class Tree {
+
+
+
+}
