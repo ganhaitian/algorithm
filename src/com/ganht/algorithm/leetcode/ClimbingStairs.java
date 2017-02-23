f1dc030cd93b02626b49788a1c65773d32811cf6
ganhaitian <ganhaitian@gmail.com>
2017-02-23 15:22:32 +0800

    移动零提交~~

---
+++ src/com/ganht/algorithm/leetcode/ClimbingStairs.java
@@ -0,0 +1,7 @@
+package com.ganht.algorithm.leetcode;
+
+/**
+ * Created by haitian.gan on 2017/1/16.
+ */
+public class ClimbingStairs {
+}
---
+++ src/com/ganht/algorithm/leetcode/LongestValidParenthese.java
@@ -0,0 +1,17 @@
+package com.ganht.algorithm.leetcode;
+
+/**
+ *
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+   For "(()", the longest valid parentheses substring is "()", which has length = 2.
+   Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+
+ * Created by haitian.gan on 2016/11/28.
+ */
+public class LongestValidParenthese {
+
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+
+}
