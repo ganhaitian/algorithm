34bfe11f1eaf28190369c0d163f23ff28ee19d74
ganhaitian <ganhaitian@gmail.com>
2017-02-24 18:10:53 +0800

    Tok K Frequent Elements 算法提交

---
+++ src/com/ganht/algorithm/leetcode/TopKFrequentElements.java
@@ -0,0 +1,27 @@
+package com.ganht.algorithm.leetcode;
+
+import java.util.List;
+
+/**
+ * Given a non-empty array of integers, return the k most frequent elements.
+ * <p>
+ * For example,
+ * Given [1,1,1,2,2,3] and k = 2, return [1,2].
+ * <p>
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ *
+ * Created by haitian.gan on 2017/2/24.
+ */
+public class TopKFrequentElements {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        return null;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
