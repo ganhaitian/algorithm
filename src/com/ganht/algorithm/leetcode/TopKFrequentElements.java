f3bb17539b01c1aa42dbfcb90add51f2b9947436
ganhaitian <ganhaitian@gmail.com>
2017-02-28 19:28:26 +0800

    Tok K Frequent Elements 算法提交

--- src/com/ganht/algorithm/leetcode/TopKFrequentElements.java
+++ src/com/ganht/algorithm/leetcode/TopKFrequentElements.java
@@ -1,6 +1,6 @@
 package com.ganht.algorithm.leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Given a non-empty array of integers, return the k most frequent elements.
@@ -17,11 +17,54 @@ import java.util.List;
 public class TopKFrequentElements {
 
     public List<Integer> topKFrequent(int[] nums, int k) {
-        return null;
+        HashMap<Integer,Integer> frequentMap = new HashMap<>();
+        HashMap<Integer,List<Integer>> frequentRank = new LinkedHashMap<>();
+        int fNum;
+        for(int num:nums){
+            if(!frequentMap.containsKey(num)){
+                frequentMap.put(num,1);
+            }else
+                frequentMap.put(num,frequentMap.get(num) + 1);
+
+            fNum = frequentMap.get(num);
+            List<Integer> lastPos = frequentRank.get(fNum - 1);
+            if(lastPos != null){
+                lastPos.remove((Integer)num);
+            }
+
+            List<Integer> nowPos = frequentRank.get(fNum);
+            if(nowPos != null){
+                nowPos.add(num);
+            }else{
+                List<Integer> poses = new ArrayList<>();
+                poses.add(num);
+                frequentRank.put(fNum,poses);
+            }
+        }
+
+        Set<Integer> keys = frequentRank.keySet();
+        Object[] ranks = keys.toArray();
+        List<Integer> result = new ArrayList<>();
+
+        int index = ranks.length - 1;
+        while(index >= 0){
+            if(result.size() >= k)
+                break;
+
+            if(frequentRank.get(ranks[index]).isEmpty()){
+                index--;
+                continue;
+            }
+
+            result.addAll(frequentRank.get(ranks[index]));
+            index--;
+        }
+
+        return result;
     }
 
     public static void main(String[] args){
-
+        new TopKFrequentElements().topKFrequent(new int[]{4,1,-1,2,-1,2,3},1);
     }
 
 }
