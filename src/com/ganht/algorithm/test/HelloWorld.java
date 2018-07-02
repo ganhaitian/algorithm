e42a13ca5e1d6b941b5091a891668cb9b75fa48e
ganhaitian@gmail.com <ganhaitian@gmail.com>
2018-07-02 23:25:17 +0800

    Merge branch 'master' of https://github.com/ganhaitian/algorithm
    Conflicts:
    	.idea/workspace.xml

---
+++ src/com/ganht/algorithm/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ganht.algorithm.test;
+
+public class HelloWorld {
+
+    private static String hello = "Hello World!";
+
+    public static void main(String[] args){
+        System.out.println(hello);
+    }
+}
